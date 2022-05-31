8f5617670fa17c388827cdb91030cd8e15efc88b
Orust <61633483+Orust@users.noreply.github.com>
2022-06-01 00:03:21 +0900

    change api line

--- main/java/com/example/youtubeclone/Common.java
+++ main/java/com/example/youtubeclone/Common.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 public class Common extends Application {
 
-    private static final String API_KEY = "AIzaSyB4GVmXhvgtj6YAoYBXwFhSu6LLXKKNbJc";
+    private static final String API_KEY = "API key";
     private final String clientId = "101466230350-tkhroar87369giah0gv49k4qhcic4fsq.apps.googleusercontent.com";
     private String code = "";
     private String access_token = "aaa";
